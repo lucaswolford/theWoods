538b088805a4a05b7a1d1d741de8060a567b0092
Lucas Wolford <lucas.wolford@gmail.com>
2015-03-31 20:34:38 -0400

    Added parallax

---
+++ Assets/scripts/ParallaxScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroll : MonoBehaviour {	
+	public float scrollSpeed;
+	private float originalX;
+	
+	void Start () {
+		originalX = transform.position.x;
+	}
+	
+	void Update () {
+		Vector3 cameraPosition = GameObject.Find("Wife").transform.position;
+
+		var offsetX = cameraPosition.x / scrollSpeed;
+
+		Vector3 theOffset = new Vector3 (offsetX + originalX, 0, 0);
+
+		transform.position = theOffset;
+	}
+}
+
