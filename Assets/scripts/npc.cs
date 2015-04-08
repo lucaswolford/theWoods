387b61e58d568c45e89a5f3d13cafbc9fb246c19
Lucas Wolford <lucas.wolford@gmail.com>
2015-04-07 21:16:52 -0400

    Added dialog pop up.

--- Assets/scripts/npc.cs
+++ Assets/scripts/npc.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class npc : MonoBehaviour {
 
 	public GameObject player;
+	private Transform dialog;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
+		dialog = transform.FindChild ("dialog");
 	}
 	
 	// Update is called once per frame
@@ -16,7 +18,10 @@ public class npc : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		if (Mathf.Abs (transform.position.x - player.transform.position.x) < 5)
-			Debug.Log ("Close");
+		if (Mathf.Abs (transform.position.x - player.transform.position.x) < 7) {
+			dialog.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+		} else {
+			dialog.localScale = new Vector3(0, 0, 0);
+		}
 	}
 }
