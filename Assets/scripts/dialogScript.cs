907f1088fe7d82b20de5161694afc3d092bcd1ae
Lucas Wolford <lucas.wolford@gmail.com>
2015-04-07 20:35:52 -0400

    Added script for the NPC.

---
+++ Assets/scripts/dialogScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dialogScript : MonoBehaviour {
+
+	public GameObject[] npcs;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		// find by tag
+		npcs = GameObject.FindGameObjectsWithTag("npc");
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject npc in npcs){
+			if (Mathf.Abs (npc.transform.position.x - player.transform.position.x) < 5)
+				Debug.Log ("Close");
+		}
+	}
+}
---
+++ Assets/scripts/npc.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npc : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		if (Mathf.Abs (transform.position.x - player.transform.position.x) < 5)
+			Debug.Log ("Close");
+	}
+}
